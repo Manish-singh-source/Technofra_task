5796adc20f8695fc0d0b0694d41f5ef1d8e1012c
saurabh <support@technofra.com>
2026-05-30 16:53:20 +0530

    fixed clients api

--- app/Http/Resources/ClientResource.php
+++ app/Http/Resources/ClientResource.php
@@ -2,12 +2,11 @@
 
 namespace App\Http\Resources;
 
-use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class ClientResource extends JsonResource
 {
-    public function toArray(Request $request): array
+    public function toArray($request): array
     {
         return [
             'id' => $this->id,
